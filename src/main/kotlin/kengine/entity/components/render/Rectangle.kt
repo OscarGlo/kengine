c7a7f817ae1ee00277ad8183db13e73fb05272cf
OscarGlo <gloaguen.oscar@gmail.com>
2023-03-18 23:44:21 +0100

    🚚 Rename RectRender to Rectangle

--- src/main/kotlin/kengine/entity/components/render/Rectangle.kt
+++ src/main/kotlin/kengine/entity/components/render/Rectangle.kt
@@ -5,6 +5,6 @@ import kengine.math.Vector2f
 import kengine.util.rectIndices
 import kengine.util.rectVertices
 
-open class RectRender(size: Vector2f, val color: Color) : Render(rectVertices(size), rectIndices) {
+open class Rectangle(size: Vector2f, val color: Color) : Render(rectVertices(size), rectIndices) {
     override fun renderSteps() = colored(2, color)
 }
